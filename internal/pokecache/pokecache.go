4611e5775d80ef5c5d6dec190edda2b394ecf35b
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2025-06-02 16:20:52 -0400

    added more to the pokecache functionality.

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,6 +1,9 @@
 package pokecache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type cacheEntry struct {
 	createdAt time.Time
@@ -9,4 +12,24 @@ type cacheEntry struct {
 
 type Cache struct {
 	cacheMap map[string]cacheEntry
+	mu       sync.Mutex
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	c.cacheMap[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.mu.Unlock()
+}
+
+func NewCache(interval time.Duration) *Cache {
+	var cache Cache
+	go cacheRun(&cache, interval)
+	return &cache
+}
+
+func cacheRun(cache *Cache, interval time.Duration) {
+	//
 }
