b21817af7ffa71d2c93ef2f5cb50954928ba2b60
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2025-06-03 15:02:59 -0400

    finished internal logic for pokecache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -17,19 +17,37 @@ type Cache struct {
 
 func (c *Cache) Add(key string, val []byte) {
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.cacheMap[key] = cacheEntry{
 		createdAt: time.Now(),
 		val:       val,
 	}
-	c.mu.Unlock()
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.cacheMap[key]
+	return entry.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for t := range ticker.C {
+		c.mu.Lock()
+		for key, value := range c.cacheMap {
+			if t.Sub(value.createdAt) >= interval {
+				delete(c.cacheMap, key)
+			}
+		}
+		c.mu.Unlock()
+	}
 }
 
 func NewCache(interval time.Duration) *Cache {
 	var cache Cache
-	go cacheRun(&cache, interval)
+	go cache.reapLoop(interval)
 	return &cache
-}
-
-func cacheRun(cache *Cache, interval time.Duration) {
-	//
 }
