b38c8858a665f8030b63e71a58e5e74ed3b2bca2
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2025-06-05 13:45:09 -0400

    added different error messages for location and pokemon http.Get requests return StatusCode 404.

--- pokeapi.go
+++ pokeapi.go
@@ -6,39 +6,39 @@ import (
 	"net/http"
 )
 
-func getData(url string, config *config, ptr any) error {
+func getData(url string, config *config, ptr any) (int, error) {
 	cachedData, ok := config.cache.Get(url)
 	if ok {
 		err := json.Unmarshal(cachedData, ptr)
 		if err != nil {
-			return fmt.Errorf("error: unable to unmarshal cached data")
+			return 0, fmt.Errorf("error: unable to unmarshal cached data")
 		}
-		return nil
+		return 0, nil
 	}
 
 	res, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("error: no response from server")
+		return 0, fmt.Errorf("error: no response from server")
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode > 299 {
-		return fmt.Errorf("response failed with status code: %d", res.StatusCode)
+		return res.StatusCode, fmt.Errorf("response failed with status code: %d", res.StatusCode)
 	}
 
 	decoder := json.NewDecoder(res.Body)
 	err = decoder.Decode(ptr)
 	if err != nil {
-		return fmt.Errorf("error decoding response body")
+		return 0, fmt.Errorf("error decoding response body")
 	}
 
 	marshaledData, err := json.Marshal(ptr)
 	if err != nil {
-		return fmt.Errorf("error: unable to marshal data")
+		return 0, fmt.Errorf("error: unable to marshal data")
 	}
 	config.cache.Add(url, marshaledData)
 
-	return nil
+	return res.StatusCode, nil
 }
 
 func getLocationAreas(config *config, isPrevious bool) error {
@@ -58,10 +58,11 @@ func getLocationAreas(config *config, isPrevious bool) error {
 		}
 	}
 
-	err := getData(url, config, &config.pokeMap)
+	_, err := getData(url, config, &config.pokeMap)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -69,7 +70,10 @@ func getAreaData(config *config, area string) error {
 	const baseURL = "https://pokeapi.co/api/v2/location-area/"
 	url := baseURL + area
 
-	err := getData(url, config, &config.pokeAreaData)
+	resCode, err := getData(url, config, &config.pokeAreaData)
+	if resCode == 404 {
+		return fmt.Errorf("Location not found!")
+	}
 	if err != nil {
 		return err
 	}
@@ -80,7 +84,10 @@ func getPokemonData(config *config, pokemon string) error {
 	const baseURL = "https://pokeapi.co/api/v2/pokemon/"
 	url := baseURL + pokemon
 
-	err := getData(url, config, &config.pokemonData)
+	resCode, err := getData(url, config, &config.pokemonData)
+	if resCode == 404 {
+		return fmt.Errorf("pokemon not found!")
+	}
 	if err != nil {
 		return err
 	}
